58caba8db65e1ff388a0e733b871608c245d360a
Bruno <bhenriques98@gmail.com>
2024-08-09 16:30:23 -0300

    projeto inicial

---
+++ exercicio.py
@@ -0,0 +1,13 @@
+print('Python na Escola de Programação da Alura.')
+print("""
+A
+L
+U
+R
+A
+""")
+nome=input('Qual o seu nome? ')
+idade = input ('Qual a sua idade? ')
+print(f'Você se chama {nome} e tem {idade} anos.')
+pi_arrendodado = 3.14159
+print(f'O valor arredondado de pi é {pi_arrendodado}.')
